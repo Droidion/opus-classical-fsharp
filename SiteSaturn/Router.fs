b67e3ff55ec2d559212baeacec21666f980fbd44
Denis Rodionov <nicholas.hawkwood@gmail.com>
2021-04-19 17:28:50 +0300

    Added keep alive

--- SiteSaturn/Router.fs
+++ SiteSaturn/Router.fs
@@ -3,8 +3,15 @@ module Site.Router
 open Saturn
 open SiteSaturn.Controllers
 
+let keepAlive = pipeline {
+    set_header "Connection" "Keep-Alive"
+    set_header "Keep-Alive" "timeout=10, max=500"
+    set_header "Strict-Transport-Security" "max-age=31536000; includeSubDomains"
+}
+
 let main =
     router {
+        pipe_through keepAlive
         forward "" index
         forward "/about" about
         forward "/composer" composer
